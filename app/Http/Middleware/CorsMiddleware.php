e8fb3e9bbaf957f15c880eb97a2f77e958be8323
An Luong <lmkhanhan@gmail.com>
2026-04-16 13:25:57 +0700

    update CorsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,9 +16,7 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $allowedOrigins = [
-            'http://localhost:5173',
-            'http://localhost:5174',
-            'http://localhost:5175',
+            'http://localhost:3000',
         ];
 
         $origin = $request->headers->get('Origin');
